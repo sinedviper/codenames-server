48d418a9927cfb3e0267cae7f0dbb45e8c98a5df
Denis Repyev <45439759+sinedviper@users.noreply.github.com>
2023-12-11 00:56:13 +0200

    add settings for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: '668077Den',
+      username: 'postgres',
+      entities: [],
+      database: 'codenames',
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
