8cff0e7387f77871cffb12679f382ef57e271a02
Denis Repyev <45439759+sinedviper@users.noreply.github.com>
2024-01-06 16:46:56 +0200

    fix bugs

--- src/words/words.module.ts
+++ src/words/words.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WordsService } from './words.service';
 import { WordsController } from './words.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WordsEntity } from './entities/words.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([WordsEntity])],
   controllers: [WordsController],
   providers: [WordsService],
 })
